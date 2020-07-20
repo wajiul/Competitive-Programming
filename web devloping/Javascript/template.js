a3f40b12884728b4af6679917ce642f0b01d1df3
wajiul <64412847+wajiul@users.noreply.github.com>
2020-07-20 20:58:14 +0600

    Create template.js

---
+++ web devloping/Javascript/template.js
@@ -0,0 +1,3 @@
+function dg(x , cnt) { for(let i = 0; i < cnt; i++) { console.log(Object.keys(x)[i] + ' : ' + Object.values(x)[i]); } }
+function watch(...a) { for(let i = 0; i < a.length; i++) { console.log(a[i]); } }
+/*--------------------------------  debug()      ------------------------------------------------------------*/
