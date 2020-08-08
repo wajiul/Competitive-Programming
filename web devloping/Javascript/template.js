82b9caab6b9594df45faca9f6cfe9938104faa4f
wajiul <64412847+wajiul@users.noreply.github.com>
2020-08-08 20:23:59 +0600

    Update template.js

--- web devloping/Javascript/template.js
+++ web devloping/Javascript/template.js
@@ -1,3 +1,8 @@
 function dg(x , cnt) { for(let i = 0; i < cnt; i++) { console.log(Object.keys(x)[i] + ' : ' + Object.values(x)[i]); } }
 function watch(...a) { for(let i = 0; i < a.length; i++) { console.log(a[i]); } }
 /*--------------------------------  debug()      ------------------------------------------------------------*/
+
+
+
+/* ---------------------------- New --------------------------------------*/
+function dg(x) { let item = Object.keys(x); let Val = Object.values(x); for(let i = 0; i < item.length; i++) { console.log(item[i] + ' : ' + Val[i]); } }
