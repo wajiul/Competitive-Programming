b07c0edca65dda4c3c6c3c48107b2a7229db0ab8
wajiul <64412847+wajiul@users.noreply.github.com>
2020-08-25 13:03:21 +0600

    Create knapsack in 2 * w space.cpp

---
+++ Problem solving/Dp/knapsack in 2 * w space.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<tuple>
+#include<string.h>
+#include <numeric> 
+#include<unordered_map>
+using namespace std; 
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define pp(...) "\033[94m [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+#define pp1(...) "\033[1;47;35m" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+#define dg(x) debug() << pp1(x)
+#define dg1(x) debug() << "[line : " << __LINE__ <<"]"<< pp(x)
+#define dg2(x,y) debug() << "[line : " << __LINE__ <<"]"<< pp(x) pp(y)
+#define dg3(x,y,z) debug() << "[line : " << __LINE__ <<"]"<< pp(x) pp(y) pp(z)
+#define dg4(w,x,y,z) debug() << "[line : " << __LINE__ <<"]"<< pp(w) pp(x) pp(y) pp(z)
+#define int long long
+#define all(x) x.begin() , x.end()
+#define clr(x , y) memset(x , y , sizeof x);
+#define sz(x) (int)x.size()
+template<typename T> void db(T x ){ debug() <<">>> " <<  x; }
+template<typename T> void db(T x , T y){ debug() <<">>> " <<  x <<" , "<< y; }
+template<typename T> void db(T x , T y , T z){ debug() <<">>> "<< x <<" , "<< y <<" , "<< z; }
+template<typename T> void db(T x , T y , T z , T a){ debug() <<">>> "<< x <<" , " << y <<" , "<< z <<" , "<< a; }
+const int xx = 2e5;
+const int Mod = 1e9 + 7;
+const int inf = 1e18;
+const int Maxn = 20010;
+
+vector<int> wt, cost;
+int n;
+int Now[2005], Nxt[2005];
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int C;
+  cin >> C >> n;
+  wt = cost = vector<int>(n,0);
+  for(int i = 0; i < n; i++) {
+    cin >> wt[i] >> cost[i];
+  }
+  for(int i = n - 1; i >= 0; i--) {
+    for(int j = 0; j <= C; j++) {
+      int res1 = 0 , res2 = 0;
+      if(j - wt[i] >= 0) {
+        res1 = Nxt[j - wt[i]] + cost[i];
+      } 
+      res2 = Nxt[j];
+      Now[j] = max(res1, res2);
+    }
+    for(int j = 0; j <= C; j++) {
+      Nxt[j] = Now[j];
+    }
+  }
+  int ans = Now[C];
+  cout << ans << '\n';
+}
