6bde9bc3de3f453b4c4c07b9e0db1435ee38aab9
wajiul <64412847+wajiul@users.noreply.github.com>
2020-09-30 14:07:21 +0600

    Update Operator overloading.cpp

--- Problem solving/Code/Operator overloading.cpp
+++ Problem solving/Code/Operator overloading.cpp
@@ -9,19 +9,49 @@ class Test {
   Test(int _M) {
     M = _M;
   }
-  Test operator+(Test &d) {
+  Test operator%(Test &d) {
     Test ob;
-    ob.M = this->M + d.M + 10;
+    ob.M = this->M % d.M;
     return ob;
   }
+    Test operator+(Test &d) {
+    Test ob;
+    ob.M = this->M + d.M;
+    return ob;
+  }
+   void operator=(int tr) {
+    Test ob;
+    M = tr;
+  }
+  friend ostream &operator<<( ostream &output, const Test &D ) { 
+  output << "->" << D.M << '\n';
+  return output;            
+}
+
+ friend istream &operator>>( istream  &input, Test &D ) { 
+  input >> D.M;
+  return input;            
+}
+  
 };
   
 int main()  
 { 
-   Test a(1);
+   Test a(11);
    Test b(4);
-   Test c;
-   c = a+b;
-   cout << c.M << '\n';
+   Test c(4);
+   Test ans;
+   ans = (a + b) % c;
+   cout << "ans = " << ans.M << '\n';
+  Test h;
+  h.M = 100;
+  cout << h.M << '\n';
+   Test g;
+   g.M = 50;
+   cout << g.M << '\n';
+   //----------------------------------
+   Test S;
+   cin >> S;
+   cout << S << '\n';
     return 0;  
 }  
