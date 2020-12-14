8974bddb43f8588914a2a886fc049bdf3fd93360
wajiul <64412847+wajiul@users.noreply.github.com>
2020-12-14 17:10:56 +0600

    Create segtree2.cpp

---
+++ Problem solving/Code/segtree2.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<tuple>
+#include<string.h>
+#include <numeric> 
+#include<unordered_map>
+using namespace std; 
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define pp(...) "\033[94m [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+#define pp1(...) "\033[1;47;35m" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+//Macro overload
+#define PP_CAT( A, B ) A ## B
+#define PP_EXPAND(...) __VA_ARGS__
+
+// Macro overloading feature support
+#define PP_VA_ARG_SIZE(...) PP_EXPAND(PP_APPLY_ARG_N((PP_ZERO_ARGS_DETECT(__VA_ARGS__), PP_RSEQ_N)))
+
+#define PP_ZERO_ARGS_DETECT(...) PP_EXPAND(PP_ZERO_ARGS_DETECT_PREFIX_ ## __VA_ARGS__ ## _ZERO_ARGS_DETECT_SUFFIX)
+#define PP_ZERO_ARGS_DETECT_PREFIX__ZERO_ARGS_DETECT_SUFFIX ,,,,,,,,,,,0
+
+#define PP_APPLY_ARG_N(ARGS) PP_EXPAND(PP_ARG_N ARGS)
+#define PP_ARG_N(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N,...) N
+#define PP_RSEQ_N 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+
+#define PP_OVERLOAD_SELECT(NAME, NUM) PP_CAT( NAME ## _, NUM)
+#define PP_MACRO_OVERLOAD(NAME, ...) PP_OVERLOAD_SELECT(NAME, PP_VA_ARG_SIZE(__VA_ARGS__))(__VA_ARGS__)
+//Overload dg_
+#define dg(...)       PP_MACRO_OVERLOAD(dg, __VA_ARGS__)
+#define dg_1(x) debug() <<"\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x)
+#define dg_2(x,y) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x) pp(y)
+#define dg_3(x,y,z) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x) pp(y) pp(z)
+#define dg_4(w,x,y,z) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z)
+#define dg_5(w,x,y,z,z1) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z) pp(z1)
+#define dg_6(w,x,y,z,z1,z2) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z) pp(z1) pp(z2)
+
+#define int long long
+#define all(x) x.begin() , x.end()
+#define clr(x , y) memset(x , y , sizeof x);
+#define sz(x) (int)x.size()
+
+#define mx 100001
+int arr[mx];
+int tree[mx * 3];
+
+void init(int node, int l, int h) {
+    if (l == h) {
+        tree[node] = arr[l];
+        return;
+    }
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (l + h) / 2;
+    init(Left, l, mid);
+    init(Right, mid + 1, h);
+    { // operation 
+       tree[node] = tree[Left] + tree[Right];
+    }
+}
+int query(int node, int l, int h, int qL, int qR) {
+
+    { // operation
+        if (qL > h || qR < l)
+            return 0; 
+        if (l >= qL && h <= qR)
+            return tree[node]; 
+    }
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (l + h) / 2;
+    int p1 = query(Left, l, mid, qL, qR);
+    int p2 = query(Right, mid + 1, h, qL, qR);
+    { // operation
+      return p1 + p2; 
+    }
+}
+void update(int node, int l, int h, int pos, int newvalue) {
+    if (pos > h || pos < l)
+        return; 
+    if (l >= pos && h <= pos) {
+        tree[node] = newvalue;
+        return;
+    }
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (l + h) / 2;
+    update(Left, l, mid, pos, newvalue);
+    update(Right, mid + 1, h, pos, newvalue);
+    { // operation
+      tree[node] = tree[Left] + tree[Right];
+    }
+}
+signed main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1;i <= n; i++) {
+        cin >> arr[i];
+    }
+    init(1, 1, n);
+    cout << query(1,1,n,1, 3) << '\n';
+    update(1, 1, n, 2, 0);
+    cout << query(1, 1, n, 1, 3) << endl;
+    update(1, 1, n, 2, 2);
+    cout << query(1, 1, n, 2, 2) << endl;
+    return 0;
+}
