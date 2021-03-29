3f1bfe632354a74b5bcbd2d752c777cf009fa8a2
wajiul <64412847+wajiul@users.noreply.github.com>
2021-03-29 12:27:01 +0600

    Create tree diameter_dp.cpp

---
+++ Problem solving/Code/tree diameter_dp.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<stdio.h>
+#include<tuple>
+#include<string.h>
+#include <numeric> 
+#include<unordered_map>
+using namespace std; 
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define pp(...) "\033[94m [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+#define pp1(...) "\033[1;47;35m" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]\033[0m"
+//Macro overload
+#define PP_CAT( A, B ) A ## B
+#define PP_EXPAND(...) __VA_ARGS__
+
+// Macro overloading feature support
+#define PP_VA_ARG_SIZE(...) PP_EXPAND(PP_APPLY_ARG_N((PP_ZERO_ARGS_DETECT(__VA_ARGS__), PP_RSEQ_N)))
+
+#define PP_ZERO_ARGS_DETECT(...) PP_EXPAND(PP_ZERO_ARGS_DETECT_PREFIX_ ## __VA_ARGS__ ## _ZERO_ARGS_DETECT_SUFFIX)
+#define PP_ZERO_ARGS_DETECT_PREFIX__ZERO_ARGS_DETECT_SUFFIX ,,,,,,,,,,,0
+
+#define PP_APPLY_ARG_N(ARGS) PP_EXPAND(PP_ARG_N ARGS)
+#define PP_ARG_N(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N,...) N
+#define PP_RSEQ_N 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+
+#define PP_OVERLOAD_SELECT(NAME, NUM) PP_CAT( NAME ## _, NUM)
+#define PP_MACRO_OVERLOAD(NAME, ...) PP_OVERLOAD_SELECT(NAME, PP_VA_ARG_SIZE(__VA_ARGS__))(__VA_ARGS__)
+//Overload dg_
+#define dg(...)       PP_MACRO_OVERLOAD(dg, __VA_ARGS__)
+#define dg_1(x) debug() <<"\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x)
+#define dg_2(x,y) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x) pp(y)
+#define dg_3(x,y,z) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(x) pp(y) pp(z)
+#define dg_4(w,x,y,z) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z)
+#define dg_5(w,x,y,z,z1) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z) pp(z1)
+#define dg_6(w,x,y,z,z1,z2) debug() << "\033[0;33mLine(" << __LINE__ <<")\033[0m"<<"\033[1;36m->"<<"\033[0m"<< pp(w) pp(x) pp(y) pp(z) pp(z1) pp(z2)
+
+#define sf(...)   PP_MACRO_OVERLOAD(sf, __VA_ARGS__)
+#define sf_1(x)   scanf("%lld",&x);
+#define sf_2(x, y) scanf("%d %d", &x,&y);
+
+// #define int long long
+#define all(x) x.begin() , x.end()
+#define clr(x , y) memset(x , y , sizeof x);
+#define sz(x) (int)x.size()
+using lint = long long int;
+const int Mod = 1e9 + 7;
+const lint inf = 1e18;
+const int N = 2e5;
+vector<vector<pair<int,int>>> g;
+vector<int> d;
+int ans;
+void dfs(int n, int p = -1) {
+  for(auto i : g[n]) {
+    if(i.first == p) continue;
+    dfs(i.first , n);
+    int b = i.second + d[i.first];
+    dg_5(n ,d[n], i.first , i.second , b);
+    ans = max(ans , b + d[n]);
+    d[n] = max(d[n] , b);
+  }
+}
+signed main() {
+  int t, cs = 0;
+  scanf("%d", &t);
+  while(t--) {
+    int n;
+    scanf("%d", &n);
+    g = vector<vector<pair<int,int>>>(n + 1);
+    d = vector<int>(n + 1, 0);
+
+    for(int i = 0;i + 1 < n; i++) {
+      int a, b, c;
+      scanf("%d %d %d", &a, &b, &c);
+      g[a].push_back({b, c});
+      g[b].push_back({a, c});
+    }
+    ans = 0;
+    dfs(1);
+    printf("Case %d: %d\n", ++cs , ans);
+  }
+}
