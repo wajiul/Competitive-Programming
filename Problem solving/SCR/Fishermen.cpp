c81cec5be29f23c1210f96aedb60dea84c89527f
Owajeul Islam <64412847+wajiul@users.noreply.github.com>
2023-06-17 17:34:50 +0600

    Update Fishermen.cpp

--- Problem solving/SCR/Fishermen.cpp
+++ Problem solving/SCR/Fishermen.cpp
@@ -1,151 +1,117 @@
-/*
-Given:
-Fishing Spots: 1 to N
-3 Gates with gate position and number of fishermen waiting to get in
-Distance between consecutive spots = distance between gate and nearest spot = 1 m
- 
-Fishermen are waiting at the gates to get in and occupy nearest fishing spot. Only 1 gate can be opened at a time and all fishermen of that gate must occupy spots before next gate is open.  
-There could be 2 spots closest to the gate. Assign only 1 spot to the last fisherman in such a way that we get minimum walking distance. For rest of the fishermen, ignore and assign any one.
- 
-Write a program to return sum of minimum distance need to walk for fishermen.
- 
-Distance is calculated as gate to nearest spot + nearest spot to closest vacant spot.
-If the gate is at position 4, then fishermen occupying spot 4 will walk 1 m,
-https://www.geeksforgeeks.org/samsung-bangalore-sri-b-interview-experience/
-*/
+#ifndef ONLINE_JUDGE
+#include "header.h"
+#else
+#include<bits/stdc++.h>
+#endif
 
-#include <iostream>
-#define INT_MAX 999999
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dg(...) 42
+#define dg_1(...) 42
+#define dg_2(...) 42
+#define dg_3(...) 42
+#endif
 using namespace std;
+#define all(x) x.begin() , x.end()
+#define clr(x , y) memset(x , y , sizeof x);
+#define sz(x) (int)x.size()
+using lint = long long int;
+#define VI vector<int>
+#define VL vector<long long>
+#define VIN(a, n) for(int i = 0;i < n;i++) {cin >> a[i];} 
+#define YN(ok) if(ok) puts("YES"); else puts("NO");
+#define PR(a,n) for(int i = 0;i < n;i++) cout << a[i] << " \n"[i == n - 1];
+#define Ts(ok) if(ok) { int t; cin >> t; while(t--) solve();} else { solve(); }
+const int Mod = 1e9 + 7;
+const lint inf = 1e18 + 10;
+const int N = 2e6;
+const int maxn = 200000;
 
-int n,ans;
-bool visited[100];
-
-struct gate{
-    int loc, men;
+struct Gate{
+    int pos, cnt_men;
 }gates[4];
 
-int findLeft(int index){
-    for(int i = index; i>0; i--){
-        if(visited[i]==false)
+int ans, n;
+bool spots[100];
+int findLeft(int p) {
+    for(int i = p;i > 0;i--) {
+        if(!spots[i]) {
             return i;
-    }
-    return INT_MAX;
-}
-
-int findRight(int index){
-    for(int i = index+1; i<=n; i++){
-        if(visited[i]==false)
-            return i;
-    }
-    return INT_MAX;
-}
-
-void solve(int x,int y, int z, int distance, int cur_gate){
-    //optimization condition, never forget
-    if(distance > ans) return;
-
-    //base case when all the mens at the current gate have occupied their places change gate
-    if(gates[cur_gate].men==0){
-        if(cur_gate==x) cur_gate=y;
-        else if(cur_gate==y) cur_gate=z;
-        // if every possible combination is seen just return the minimum distance
-        else{
-            ans=min(ans,distance);
-            return ;
         }
     }
-
-    //recursive case
-    //searching for vacant spot at the left of current gate
-    int l = findLeft(gates[cur_gate].loc);
-    //searching for vacant spot at the right of current gate
-    int r = findRight(gates[cur_gate].loc);
-
-    //cost of occupying the left vacant spot
-    int costL = abs(gates[cur_gate].loc - l) + 1;
-    //cost of occupying the right vaacant spot
-    int costR = abs(gates[cur_gate].loc - r) + 1;
-
-    bool goRight=true, goLeft=true;
-    if(costL > costR) goLeft = false;
-    if(costR > costL) goRight = false;
-
-    if(goLeft){
-        visited[l]=true;
-        gates[cur_gate].men--;
-        solve(x,y,z,distance + costL, cur_gate);
-        //backtracking
-        visited[l]=false;
-        gates[cur_gate].men++;
-    } 
-
-    if(goRight){
-        visited[r]=true;
-        gates[cur_gate].men--;
-        solve(x,y,z,distance + costR, cur_gate);
-        //backtracking
-        visited[r]=false;
-        gates[cur_gate].men++;
-    }
+    return maxn;
 }
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        cin>>n;
-
-        //enter data for the positions and mens at the gate
-        for(int i=1; i<=3; i++)
-            cin>>gates[i].loc>>gates[i].men;
-
-        for(int i=0; i<100; i++)
-            visited[i]=false;
-        
-        ans=INT_MAX;
-        //checking for all possible permutations of gate opening
-        solve(1,2,3,0,1);
-        solve(1,3,2,0,1);
-        solve(2,1,3,0,2);
-        solve(2,3,1,0,2);
-        solve(3,1,2,0,3);
-        solve(3,2,1,0,3);
-        
-        cout<<ans<<endl;
-        
+int findRight(int p) {
+    for(int i = p + 1;i <= n;i++) {
+        if(!spots[i]) {
+            return i;
+        }
     }
-    return 0;
+    return maxn;
 }
 
- 
-/* TestCases
-5
-10
-4 5
-6 2
-10 2
-10
-8 5
-9 1
-10 2
-24
-15 3
-20 4
-23 7
-39
-17 8
-30 5
-31 9
-60
-57 12
-31 19
-38 16
- 
-outputs
-18
-25
-57
-86
-339
-*/
+void solve(vector<int> p, int cur_gate, int cost) {
+    if(cost > ans) return;
+    if(gates[cur_gate].cnt_men == 0) {
+		if(cur_gate == p.back()) {
+			ans = min(ans, cost);
+			return;
+		}
+		for(int i = 0;i + 1 < (int)p.size();i++) {
+			if(cur_gate == p[i]) {
+				cur_gate = p[i + 1];
+				break;
+			}
+		}
+    }
+
+    int L = findLeft(gates[cur_gate].pos);
+    int R = findRight(gates[cur_gate].pos);
+    int cL = abs(gates[cur_gate].pos - L) + 1;
+    int cR = abs(gates[cur_gate].pos - R) + 1;
+    bool goLeft = true, goRight = true;
+
+    if(cL > cR) goLeft = false;
+    if(cR > cL) goRight = false;
+
+    if(goLeft) {
+        spots[L] = true;
+        gates[cur_gate].cnt_men--;
+        solve(p ,cur_gate, cost + cL);
+        spots[L] = false;
+        gates[cur_gate].cnt_men++;
+    }
+
+    if(goRight) {
+        spots[R] = true;
+        gates[cur_gate].cnt_men--;
+        solve(p, cur_gate, cost + cR);
+        spots[R] = false;
+        gates[cur_gate].cnt_men++;
+    }
+}
+void solve() {
+    
+    cin >> n;
+    for(int i = 1;i <= 3;i++) {
+        cin >> gates[i].pos >> gates[i].cnt_men;
+    }
+    vector<int> g = {1, 2, 3};
+    for(int i = 0;i < 100;i++) {
+        spots[i] = false;
+    }
+    ans = maxn;   
+    do {
+        solve(g, g[0], 0);
+        dg(ans, g);  
+    } while(next_permutation(g.begin(), g.end())); 
+
+    cout << ans << '\n';
+}
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Ts(true);
+}
